0de63589ba1bb799be39231500848189b9c3fc64
BrandonDoran <bdoran@gmail.com>
2013-09-25 16:53:59 -0700

    user/micropost association refinements

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,5 @@
 class Micropost < ActiveRecord::Base
 	belongs_to :user
-	validates :user_id, presence: true
+	default_scope -> { order('created_at DESC') }
+  validates :user_id, presence: true
 end
